1e8b9afe07befea1b988f3bc968d37b20540c530
Bijit-Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2020-10-30 11:51:07 +0530

    Add files via upload

---
+++ includes/foot.php
@@ -0,0 +1,14 @@
+<?php include'ucweb.php';?>
+<?php
+include 'includes/info.php';
+?>
+<div class="group" align="center">
+<span class="page">External Links:</span>
+<br/>
+<a href="http://<?php echo ''.$host.'';?>" title="Home Page">Home Page</a> | <a href="<?php echo ''.$facebook.'';?>" target="_blank" title="Like Us On Facebook">Facebook</a> | <a href="<?php echo ''.$twitter.'';?>" target="_blank" title="Follow Us On Twitter">Twitter</a>
+</div>
+</div><div class="group" align="center"><div id="footer" align="center">
+&copy; <?php echo date("Y") ?> <?php echo ''.$sitename.'';?>
+</div>
+</body>
+</html>
